425820400ace092c50a11063bfbe018c9af48cc3
Pieter Hoekstra <pieterhoekstra@gmail.com>
2022-01-29 16:19:05 +0000

    Allow upload up to 2GB

--- files/php/php.sh
+++ files/php/php.sh
@@ -2,8 +2,8 @@
 
 # Change max upload size
 ini=$(php -r 'echo php_ini_loaded_file();')
-sudo sed -i 's/^upload_max_filesize.*/upload_max_filesize = 100MB/' ${ini}
+sudo sed -i 's/^upload_max_filesize.*/upload_max_filesize = 2G/' ${ini}
 
 replace="fpm"
 fpm="${ini/cli/"$replace"}"
-sudo sed -i 's/^upload_max_filesize.*/upload_max_filesize = 100MB/' ${fpm}
+sudo sed -i 's/^upload_max_filesize.*/upload_max_filesize = 2G/' ${fpm}
