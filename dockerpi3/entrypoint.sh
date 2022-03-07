ca361686ee9eac21559762bff234c6db358a5951
Jacco Logtenberg <jacco@tetter.nl>
2022-03-07 20:42:49 +0100

    dockerpi3 moved from dedicated repo

---
+++ dockerpi3/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Copying sdcard image to volume sd ... "
+cp /*-raspios-bullseye-armhf-lite.img /sd/sdcard.img
+echo "DONE"
+
+echo -n "Resizing sdcard image to 2G ... "
+qemu-img resize -f raw /sd/sdcard.img 2G
+
+echo -n "Copying kernel and Broadcom 2710 firmware to volume sd/kernel ... "
+mkdir /sd/kernel
+cp /kernel8.img bcm2710-rpi-3-b-plus.dtb /sd/kernel
+echo "DONE"
+
+exit 0
